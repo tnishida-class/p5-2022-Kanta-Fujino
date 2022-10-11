78b2ec30d09ddfe58ad35f1e1b59f4d71476569c
Kanta-Fujino <114969267+Kanta-Fujino@users.noreply.github.com>
2022-10-11 12:54:52 +0900

    section2-q2 Update

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,20 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      fill((j+i)%2==1?125:250);
+      rect(i*25,j*25,25,25);
+      
+
+      if(j<3&&(i+j)%2==1){
+        fill(225,0,0)
+        ellipse(12.5+25*i,12.5+25*j,23)}
+     else if (4<j&&(i+j)%2==1){
+      fill(0,0,0)
+      ellipse(12.5+25*i,12.5+25*j,23)}
+     }
+      
+
+      
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
-}
