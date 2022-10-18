a18e91f66914b3e0ee38e47e59b4f2ac4d5f056b
Kanta-Fujino <114969267+Kanta-Fujino@users.noreply.github.com>
2022-10-18 11:51:24 +0900

    section3-q1 Update

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,5 +12,26 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  ellipse(width / 2, height / 2, 20);
+
+  if(keyIsDown("A".charCodeAt(0))){
+    if(count<10){
+      ellipse(width / 2, height / 2, 50)
+   }
+   else if(10<=count<20){
+     ellipse(width / 2, height / 2, 100)
+     count=0
+   }
+  }
+  
+
+  else  {
+    if(count<50){
+      ellipse(width / 2, height / 2, 50) 
+    }
+    else if(50<count<=100){
+      ellipse(width / 2, height / 2, 100) 
+      count=0 
+    }
+  }
 }
