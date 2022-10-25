da1b36572b04e4f3ec89573869949dc619378885
Kanta-Fujino <114969267+Kanta-Fujino@users.noreply.github.com>
2022-10-25 12:06:54 +0900

    section4-1 Update

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -20,16 +20,28 @@ function setup(){
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
+average = sum / scores.length
+
+console.log(average)
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+  if(largest <scores[i]){
+    largest=scores[i]
   }
+}
+console.log("最大値",largest)
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
     // BLANK[3]　ヒント：最小値とだいたい同じ
-  }
+   if(smallest > scores[i]){
+    smallest = scores[i]
+   }  
+ }
+
+ console.log("最小値",smallest)
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
@@ -41,10 +53,35 @@ function setup(){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
+
+    if(largest==scores[i]){
+    fill(225,0,0)
     rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
 
+    else if(smallest==scores[i]){
+    fill(0,0,225)
+    rect(i * dx + 2, height - h, dx - 4, h);
+    fill(0);
+    text(scores[i].toPrecision(3), i * dx, height - h);
+  }
+
+  else{
+    fill(121,121,121);
+    rect(i * dx + 2, height - h, dx - 4, h);
+    fill(0);
+    text(scores[i].toPrecision(3), i * dx, height - h);
+  }
+
+  }
   // BLANK[5] 平均点の線を引きます
+  
+  const hh = height * average / 100;
+  fill(0);
+  text(average.toPrecision(3),10, height - hh);
+  stroke(0,225,0);
+  line(0,height-hh,400,height-hh)
+  
 }
